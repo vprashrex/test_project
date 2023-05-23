d884636f128d02ffc23f9f0510599f09b769d674
vprashrex <71649489+vprashrex@users.noreply.github.com>
2023-05-23 11:21:52 +0530

    to deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1>
-        HELLO WORLD THIS IS AZURE
+        PRASHANT VASUDEVAN
       </h1>
       </header>
       
